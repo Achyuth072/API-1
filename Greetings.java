5decfb2314064fdd7ccd6d6befd3a5a1bb2d9a5b
Achyuth Suchit <achyuthsuchit@gmail.com>
2022-03-08 11:35:39 +0530

    Added Greetings.java file

---
+++ Greetings.java
@@ -0,0 +1,38 @@
+package com.ba.SpingBootDataJPAdemo;
+
+public class Greetings {
+	
+	private int id;
+	private String content;
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+//	public Greetings(int id, String content) {		
+//		this.id = id;
+//		this.content = content;
+//	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
