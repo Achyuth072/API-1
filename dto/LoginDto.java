00207194a22427f896189fcb7ed00b4c0ac13a28
Achyuth Suchit <achyuthsuchit@gmail.com>
2022-03-08 11:05:16 +0530

    Added LoginApp files

---
+++ dto/LoginDto.java
@@ -0,0 +1,22 @@
+package com.ba.LoginApp.dto;
+
+public class LoginDto {
+	
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String u_name) {
+		this.username = u_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
